c9d364a240dbacc5248f4307d1153609b00a6a2d
Venkateshwaran S <54279094+venkatesh1996@users.noreply.github.com>
2024-06-09 19:59:39 +0530

    first commit

---
+++ src/main/java/edu/fanshawe/pawsomecare/PawsomecareApplication.java
@@ -0,0 +1,13 @@
+package edu.fanshawe.pawsomecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsomecareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsomecareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/fanshawe/pawsomecare/PawsomecareApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fanshawe.pawsomecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsomecareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
